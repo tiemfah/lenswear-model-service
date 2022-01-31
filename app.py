9cda9a734fc7715a822ee6fae64760a7c64c2a5e
wodwine <wodwine@hotmail.com>
2022-01-31 20:52:43 +0700

    add cloth_matching

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ from typing import Dict, List
 from flask import Flask, Response, request
 from google.cloud import storage
 from keras import models
+
 from keras.preprocessing.image import img_to_array
 from numpy import expand_dims
 from PIL import Image
+from skimage import metrics
+from scipy.linalg import norm
+from scipy import sum, average
+import cv2
 
 app = Flask(__name__)
 model = None
+
 
 def download_blob(bucket_name):
     blobs = storage.Client(project="lenswear-service").bucket(bucket_name).list_blobs()
@@ -25,10 +31,12 @@ def download_blob(bucket_name):
         blob.download_to_filename(destination_uri)
         print(f'Finish downloading {blob.name}.')
 
+
 def load_models():
     global model
     model = models.load_model("assets/apparel_list.h5")
     print('Model loaded.')
+
 
 def load_classes():
     global apparel_ids
@@ -36,7 +44,35 @@ def load_classes():
         apparel_ids = load(f)
     print('Classes loaded.')
 
-def cloth_matching(classification_results: List, img: Image) -> Dict[str, float]:
+
+def normalize(arr):
+    rng = arr.max() - arr.min()
+    amin = arr.min()
+    return (arr - amin) * 255 / rng
+
+
+def compare_images(img1, img2):
+    # normalize to compensate for exposure difference, this may be unnecessary
+    img1 = normalize(img1)
+    img2 = normalize(img2)
+    # calculate the difference and its norms
+    diff = img1 - img2  # elementwise for scipy arrays
+    m_norm = sum(abs(diff))  # Manhattan norm
+    z_norm = norm(diff.ravel(), 0)
+    return m_norm, z_norm
+
+
+def to_grayscale(arr):
+    """
+    If arr is a color image (3D array), convert it to grayscale (2D array).
+    """
+    if len(arr.shape) == 3:
+        return average(arr, -1)  # average over the last axis (color channels)
+    else:
+        return arr
+
+
+def cloth_matching(image, filenames: List) -> Dict[str, float]:
     """
     WRITE YOUR FUNCTION HERE
     WRITE YOUR FUNCTION HERE
@@ -46,11 +82,21 @@ def cloth_matching(classification_results: List, img: Image) -> Dict[str, float]
     WRITE YOUR FUNCTION HERE
     WRITE YOUR FUNCTION HERE
     params:
-        results -> classification result from your model
+        image -> 3D numpy array of query image
     return:
-        List[str] of possible apparel ids in database
+        filenames -> List[str] of possible apparel paths in database
     """
-    return {"apparel_shirt_1": 0.6, "apparel_shirt_2": 0.4}
+    image = to_grayscale(image.astype(float))
+    image = cv2.resize(image, (600, 600))
+    matching_dict = {}
+    img_list = [imread(filename) for filename in filenames]
+    for i, img in enumerate(img_list):
+        img = to_grayscale(img.astype(float))
+        img = cv2.resize(img, (600, 600))
+        score, diff = metrics.structural_similarity(image, img, full=True)
+        matching_dict[filenames[i]] = score
+    return matching_dict
+
 
 def preprocessing_image(img, target_size):
     if img.mode != "RGB":
@@ -59,6 +105,7 @@ def preprocessing_image(img, target_size):
     img = img_to_array(img)
     img = expand_dims(img, axis=0)
     return img
+
 
 def init_model_server():
     # download_blob("lenswear-service-model")
@@ -71,6 +118,7 @@ def init_model_server():
 def index():
     return Response(status=200)
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
     response = {'success': False}
@@ -82,6 +130,7 @@ def predict():
     response['predictions'] = matching_result
     response['success'] = True
     return response, 200
+
 
 if __name__ == '__main__':
     init_model_server()
